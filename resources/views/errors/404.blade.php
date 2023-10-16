e401831487a18a573c6976cd09d93322af2110e2
mostafizur-pro <mostafizur0195@gmail.com>
2023-10-16 10:38:33 +0600

    404 error page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <style type="text/css">
+        body {
+            background-color: #f7f7f7;
+        }
+        .error-container {
+            text-align: center;
+            margin-top: 100px;
+        }
+        .error-main {
+            background-color: #ffffff;
+            /* box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.1); */
+            padding: 20px;
+        }
+        .error-main h1 {
+            font-weight: bold;
+            color: #333;
+            font-size: 200px;
+            /* text-shadow: 2px 2px 4px #888; */
+        }
+        .error-main h2 {
+            color: #555;
+            font-size: 30px;
+        }
+        .error-main p {
+            color: #777;
+            font-size: 18px;
+        }
+        .error-link {
+            text-decoration: none;
+            color: #007bff;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <div class="error-container">
+            <div class="error-main">
+                <h1>404</h1>
+                <h2>Page not found</h2>
+                <p>Sorry, the page you are looking for does not exist.</p>
+                <p>Go back to <a href="/" class="error-link">businesseprofile.com</a></p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
