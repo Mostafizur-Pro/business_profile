918d2c2e9e57fa3cb694a336c62125eecef4dcff
mostafizur-pro <mostafizur0195@gmail.com>
2023-11-02 11:09:50 +0600

    faq page edit

--- resources/views/components/faq/faqList.blade.php
+++ resources/views/components/faq/faqList.blade.php
@@ -4,31 +4,41 @@
         <p class="lead">Explore answers to common questions about our services.</p>
     </header>
 
-
     <section class="py-5">
-            <div class="container">
-                <h2 class="mb-4">Frequently Asked Questions</h2>
-                <div class="accordion" id="faqAccordion">
-                    <div class="card">
-                        <div class="card-header" id="faqHeading1">
-                            <h2 class="mb-0">
-                                <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#faqCollapse1" aria-expanded="true" aria-controls="faqCollapse1">
-                                    What is Business Profile?
-                                </button>
-                            </h2>
-                        </div>
-
-                        <div id="faqCollapse1" class="collapse show" aria-labelledby="faqHeading1" data-parent="#faqAccordion">
-                            <div class="card-body">
-                                Business Profile is a project of IITAB (International IT Association of Bangladesh) that aims to bring all legal businesses to the forefront. Our goal is to introduce businesses and their owners to every corner of Bangladesh and the world, from remote areas to urban giants, by centralizing them in one place.
-                            </div>
+        <div class="container">
+            <h2 class="mb-4">Frequently Asked Questions</h2>
+            <div class="" id="faqAccordion">
+                <!-- Add your FAQ items with the same structure and CSS classes as before -->
+                <div class="card">
+                    <div class="card-header" id="faqHeading1">
+                        <h2 class="mb-0">
+                            <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#faqCollapse1" aria-expanded="true" aria-controls="faqCollapse1">
+                                What is Business Profile?
+                            </button>
+                        </h2>
+                    </div>
+                    <div id="faqCollapse1" class="collapse show" aria-labelledby="faqHeading1" data-parent="#faqAccordion">
+                        <div class="card-body">
+                            Business Profile is a project of IITAB (International IT Association of Bangladesh) that aims to bring all legal businesses to the forefront. Our goal is to introduce businesses and their owners to every corner of Bangladesh and the world, from remote areas to urban giants, by centralizing them in one place.
                         </div>
                     </div>
-
-                    <!-- Add more FAQ items following the same structure -->
                 </div>
+                <div class="card">
+                    <div class="card-header" id="faqHeading1">
+                        <h2 class="mb-0">
+                            <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#faqCollapse1" aria-expanded="true" aria-controls="faqCollapse1">
+                                What is Business Profile?
+                            </button>
+                        </h2>
+                    </div>
+                    <div id="faqCollapse1" class="collapse show" aria-labelledby="faqHeading1" data-parent="#faqAccordion">
+                        <div class="card-body">
+                            Business Profile is a project of IITAB (International IT Association of Bangladesh) that aims to bring all legal businesses to the forefront. Our goal is to introduce businesses and their owners to every corner of Bangladesh and the world, from remote areas to urban giants, by centralizing them in one place.
+                        </div>
+                    </div>
+                </div>
+                <!-- Add more FAQ items following the same structure -->
             </div>
-        </section>
-
-
+        </div>
+    </section>
 </div>
