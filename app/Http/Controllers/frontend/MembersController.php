b029aea219ea4ce4b5c20335607ab028df6daf84
mostafizur-pro <mostafizur0195@gmail.com>
2023-11-15 18:54:32 +0600

    our old client list show

--- app/Http/Controllers/frontend/MembersController.php
+++ app/Http/Controllers/frontend/MembersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\frontend;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class MembersController extends Controller
 {
@@ -12,9 +13,37 @@ class MembersController extends Controller
         return view('layouts/members/ourClients');
     }
 
-    public function our_old_clients()
+    public function our_old_clients(Request $request)
     {
-        return view('layouts/members/ourOldClients');
+        $perPage = $request->input('perPage', 10);
+
+        $search = $request->input('search');
+        $letter = $request->input('letter'); // Add 'letter' to the request parameters
+
+        $query = DB::table('user_login_information')
+            ->select('*')
+            ->orderBy('owner_name', 'asc');
+
+        if ($letter) {
+            // Filter results by the selected letter
+            $query->where('owner_name', 'LIKE', $letter . '%');
+        } else {
+            // Perform text-based search if 'letter' is not specified
+            $query->when($search, function ($query) use ($search) {
+                $query->where('employee_id', 'LIKE', '%' . $search . '%')
+                    ->orWhere('owner_name', 'LIKE', '%' . $search . '%')
+                    ->orWhere('email', 'LIKE', '%' . $search . '%')
+                    ->orWhere('organization_name', 'LIKE', '%' . $search . '%')
+                    ->orWhere('contact_number', 'LIKE', '%' . $search . '%')
+                    ->orWhere('address', 'LIKE', '%' . $search . '%');
+            });
+        }
+        $data = $query->paginate($perPage)->onEachSide(1)->withQueryString();
+        // dd($query);
+        // dd($data);
+
+        // return view('clientList', ['data' => $data]);
+        return view('layouts/members/ourOldClients', ['data' => $data]);
     }
 
     public function our_employees()
--- resources/views/layouts/members/ourOldClients.blade.php
+++ resources/views/layouts/members/ourOldClients.blade.php
@@ -10,7 +10,21 @@
     @include('components/cover/coverText')
     <!-- Cover Page -->
 
-    About Page
+    <h2 class="my-12 text-3xl font-bold text-center">
+        Meet the <u class="text-primary dark:text-primary-400">team</u>
+    </h2>
+    <div>
+
+        <div> Side </div>
+        <div>
+
+            <!-- Container for demo purpose -->
+            @include('components/member/ourOldClient')
+            <!-- Container for demo purpose -->
+        </div>
+    </div>
+
+
 </div>
 
 @endsection
