899d70268f1ce3c75f31c918fd007a78b5b1f5a3
mostafizur-pro <mostafizur0195@gmail.com>
2023-12-05 18:47:15 +0600

    user post create table

---
+++ database/migrations/2023_12_05_115417_create_user_post_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserPostTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_post', function (Blueprint $table) {
+            $table->id();
+            $table->string('post');
+            $table->string('image')->nullable();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('user_info')->onDelete('cascade');
+            $table->string('role');
+            $table->json('like')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_post');
+    }
+}
